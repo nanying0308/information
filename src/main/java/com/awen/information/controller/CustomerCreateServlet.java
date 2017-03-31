86b14220188cc2bf05bf872a5257df55936f7044
lianglianwen <1011688430@qq.com>
2017-03-31 11:42:07 +0800

    llw

---
+++ src/main/java/com/awen/information/controller/CustomerCreateServlet.java
@@ -0,0 +1,37 @@
+package com.awen.information.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by llw on 2017/3/30.
+ */
+
+/**
+ * create customer
+ */
+@WebServlet("customer_create")
+public class CustomerCreateServlet extends HttpServlet {
+    /**
+     * step into create customer page
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    /**
+     * deal create customer request
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
